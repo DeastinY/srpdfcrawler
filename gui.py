de4dcdfe218a4434608c4ecad06fd1cd40afa86b
DeastinY <polzin.richard@gmail.com>
2017-04-18 21:10:23 +0200

    Now sometimes shows results. Still reasonable slow

--- gui.py
+++ gui.py
@@ -1,22 +1,25 @@
 import os
 import sys
 import configparser
+from pdf_search import Searcher
 from pdf_parser import parse
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QMessageBox, QHBoxLayout, QGroupBox, QDialog, QVBoxLayout, QPushButton, QLineEdit, QLabel
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QMessageBox, QHBoxLayout, QGroupBox, QVBoxLayout, QLineEdit, QLabel
 
 
 class App(QWidget):
  
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 file dialogs - pythonspot.com'
+        self.title = 'SR PDF Searcher'
         self.left = 100
         self.top = 100
         self.width = 640
         self.height = 480
         self.config = configparser.ConfigParser()
         self.config_file = 'config.ini'
+        self.readConfig()
+        self.searcher = Searcher(self.config['GENERAL']['RulebookLocation'])
         self.initUI()
 
     def initUI(self):
@@ -26,8 +29,8 @@ class App(QWidget):
         windowLayout = QVBoxLayout()
         windowLayout.addWidget(self.horizontalGroupBox, 1)
         windowLayout.addWidget(self.contentArea, 100)
+        windowLayout.setAlignment(self.contentArea, Qt.AlignTop)
         self.setLayout(windowLayout)
-        self.readConfig()
 
         self.show()
 
@@ -39,7 +42,7 @@ class App(QWidget):
         self.lineEdit = QLineEdit(self)
         layout.addWidget(self.lineEdit)
 
-        self.lineEdit.textChanged.connect(self.search)
+        self.lineEdit.editingFinished.connect(self.search)
  
         self.horizontalGroupBox.setLayout(layout)
         self.contentArea = QLabel(self)
@@ -49,7 +52,12 @@ class App(QWidget):
             self.search()
 
     def search(self):
-        print(self.lineEdit.text())
+        print('Search started')
+        results = self.searcher.search(self.lineEdit.text())
+        text = ["Found {} results\n------------------------\n".format(len(results))]
+        text.extend([s[0]+" : \t"+' '.join(s[1]) for s in results])
+        print(text)
+        self.contentArea.setText('\n'.join(text))
 
     def readConfig(self):
         if not os.path.exists(self.config_file):
@@ -59,13 +67,13 @@ class App(QWidget):
                 self.config.write(confout)
         self.config.read(self.config_file)
 
- 
     def openPdfDialog(self):    
         QMessageBox.question(self, "Process rulebooks", "Not set up yet. Select directory containing PDF files to process. This may take up to 5 minutes", QMessageBox.Ok)
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         directoryName = QFileDialog.getExistingDirectory(self, options=options)
         if directoryName:
+            parse(directoryName)
             return directoryName
 
  
---
+++ pdf_search.py
@@ -0,0 +1,25 @@
+import os
+from fuzzywuzzy import process
+
+
+class Searcher:
+    def __init__(self, directory):
+        self.pdf_folder = directory
+        self.content = self.read(directory)
+
+    def search(self, term):
+        return [(k, v) for k, v in self.content.items() if term.lower() in ' '.join(v).lower()]
+        #return [(k, process.extract(term, v)) for k, v in self.content.items()]
+
+    def read(self, directory):
+        content = {}
+        for root, dirs, files in os.walk(directory):
+            for f in files:
+                if '.pdf' in f.lower() and '.txt' in f.lower():
+                    with open(os.path.join(root, f), 'r', encoding='utf-8') as fin:
+                        lines = fin.readlines()
+                        content[f] = lines
+        return content
+
+
+
